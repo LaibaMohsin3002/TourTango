3d1967a1b13cf603825fb23376837db02eb4213f
Aniqa Azhar <aniqaazhar99@gmail.com>
2025-05-04 22:29:12 +0500

    add models

--- lib/widgets/carousel.dart
+++ lib/widgets/carousel.dart
@@ -1,7 +1,7 @@
 // widgets/carousel_slider_widget.dart
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-import 'packages.dart';
+import '../models/packages.dart';
 
 class CarouselSliderWidget extends StatelessWidget {
   final List<Package> packages;
